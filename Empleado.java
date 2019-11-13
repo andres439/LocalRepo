acec8e858228cc486464058879414648eb80b535
Andres Murillo <joseandres9710@hotmail.com>
2019-11-13 14:29:41 -0600

    hecho por andres,commit inicial.

---
+++ Empleado.java
@@ -0,0 +1,14 @@
+public class Empleado {
+	private String nombre;
+	private String apellido;
+	private double sueldo;
+
+
+	public Empleado() {
+	}
+
+
+	public String consultaNombreCompleto(String nombre, String apellido) {
+		return "";
+	}
+}
